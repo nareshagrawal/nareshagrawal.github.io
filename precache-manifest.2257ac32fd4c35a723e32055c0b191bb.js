86a5b28f67f1169f767d3c990e5c0b97b0e02719
nareshagrawal <nareshagrawal316@gmail.com>
2021-05-01 19:15:39 -0400

    Updates

---
+++ precache-manifest.2257ac32fd4c35a723e32055c0b191bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f55004972a91fa7cb469b9a34a2ca5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0cf14343c31ab91dfe21",
+    "url": "/static/css/main.f5497eb8.chunk.css"
+  },
+  {
+    "revision": "1390d13147fcc8767529",
+    "url": "/static/js/2.95a996af.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.95a996af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0cf14343c31ab91dfe21",
+    "url": "/static/js/main.78bc8605.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/home/precache-manifest.11424f3444979424003fa0d89c1fc5f3.js"
+  "/precache-manifest.2257ac32fd4c35a723e32055c0b191bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
