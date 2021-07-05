3cb4a45a39b5c5818bfd94af6a169745cec6c802
nareshagrawal <nareshagrawal316@gmail.com>
2021-07-05 09:28:26 -0400

    change project card & skills

--- src/components/home/ProjectCard.jsx
+++ src/components/home/ProjectCard.jsx
@@ -44,7 +44,7 @@ const CardButtons = ({ svn_url }) => {
         href={`${svn_url}/archive/master.zip`}
         className="btn btn-outline-secondary mr-3"
       >
-        <i className="fab fa-github" /> Clone Project
+        <i className="fab fa-github" /> Clone
       </a>
       <a href={svn_url} target=" _blank" className="btn btn-outline-secondary">
         <i className="fab fa-github" /> Repository
