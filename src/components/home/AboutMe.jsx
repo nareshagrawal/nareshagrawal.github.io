9e4eff608932170a1766348843010f796ebac791
nareshagrawal <nareshagrawal316@gmail.com>
2021-08-19 10:39:45 -0400

    removing useeffect from about section

--- src/components/home/AboutMe.jsx
+++ src/components/home/AboutMe.jsx
@@ -1,33 +1,33 @@
 import React from "react";
-import axios from "axios";
+// import axios from "axios";
 
-const pictureLinkRegex = new RegExp(
-  /[(http(s)?):(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/
-);
+// const pictureLinkRegex = new RegExp(
+//   /[(http(s)?):(www.)?a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/
+// );
 
 const AboutMe = ({ heading, message, link, imgSize, resume }) => {
-  const [profilePicUrl, setProfilePicUrl] = React.useState("");
+  const [profilePicUrl, setProfilePicUrl] = React.useState(link);
   const [showPic, setShowPic] = React.useState(Boolean(link));
 
-  React.useEffect(() => {
-    if (link && !pictureLinkRegex.test(link)) {
-      handleRequest();
-    } else {
-      setProfilePicUrl(link);
-    }
-  }, [link]);
+  // React.useEffect(() => {
+  //   if (link && !pictureLinkRegex.test(link)) {
+  //     handleRequest();
+  //   } else {
+  //     setProfilePicUrl(link);
+  //   }
+  // }, [link]);
 
-  const handleRequest = async () => {
-    const instaLink = "https://www.instagram.com/";
-    const instaQuery = "/?__a=1";
-    try {
-      const response = await axios.get(instaLink + link + instaQuery);
-      setProfilePicUrl(response.data.graphql.user.profile_pic_url_hd);
-    } catch (error) {
-      setShowPic(false);
-      console.error(error.message);
-    }
-  };
+  // const handleRequest = async () => {
+  //   const instaLink = "https://www.instagram.com/";
+  //   const instaQuery = "/?__a=1";
+  //   try {
+  //     const response = await axios.get(instaLink + link + instaQuery);
+  //     setProfilePicUrl(response.data.graphql.user.profile_pic_url_hd);
+  //   } catch (error) {
+  //     setShowPic(false);
+  //     console.error(error.message);
+  //   }
+  // };
 
   return (
     <div id="aboutme" className="jumbotron jumbotron-fluid m-0">
